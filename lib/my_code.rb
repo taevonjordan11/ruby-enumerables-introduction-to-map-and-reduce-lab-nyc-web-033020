4b9a6e0028a7c0a3e5ce770baf416cd999a38550
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-17 15:55:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -48,3 +48,13 @@ def reduce_to_total(source_array, starting_point=0)
   sum
 end
 
+def reduce_to_all_true(source_array)
+  counter = 0
+  while counter < source_array.length do
+    if !source_array[counter]
+      return false
+    end
+    counter+=1
+  end
+  return true
+end
