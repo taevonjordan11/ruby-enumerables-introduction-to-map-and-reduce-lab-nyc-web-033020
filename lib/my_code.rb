0845e1ea82f6090171fd7bbdf9530052e00e7544
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-17 15:54:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,3 +38,12 @@ def map_to_square(source_array)
   new_array
 end
 
+def reduce_to_total(source_array, starting_point=0)
+  sum=starting_point
+  counter = 0
+  while counter < source_array.length do
+    sum+=source_array[counter]
+    counter+=1
+  end
+  sum
+end
